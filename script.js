b27bb6d9c5ac3335e1ca456190e90f012f76c38e
jake-g <omonoid@gmail.com>
2023-04-13 20:06:27 -0700

    use playlists rather than full album videos, header fixed at top and collapse info by default
    lazy load
    
    undo lazy load tmp
    
    simple lazy loading attmpt
    
    best lazy load?
    
    update with more allow
    
    untested pause

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var players = [];
+function onYouTubeIframeAPIReady() {
+  var playerContainers = document.getElementsByClassName('playerContainer');
+  for (var i = 0; i < playerContainers.length; i++) {
+    var playlistId = playerContainers[i].getAttribute('data-playlist-id');
+    var player = new YT.Player(playerContainers[i], {
+
+      playerVars: {
+        'listType': 'playlist',
+        'list': playlistId,
+        'autoplay': 0,        // 1, video will automatically play when loaded
+        'controls': 1,        // 1, display player controls like play, pause, and volume
+        'rel': 0,             // 1, related videos will be shown at the end of a video playback
+        'showinfo': 0,        // 1, display video information like video title and uploader's username
+        'modestbranding': 1,  // 1, dont load youtube log
+      },
+    });
+    players.push(player);
+    player.addEventListener('play', function() {
+      pauseOtherPlayers(this);
+    });
+  }
+}
+
+function pauseOtherPlayers(currentPlayer) {
+  for (var i = 0; i < players.length; i++) {
+    if (players[i] !== currentPlayer) {
+      players[i].pauseVideo();
+    }
+  }
+}
+
+
+// // Buttons toggled onload
+// $(document).ready(function () {
+//   $("#collapse-main").trigger("click");
+// });
+
+// Buttons
+$(document).ready(function () {
+  $("#collapse-main").click(function () {
+    $("main").slideToggle("slow");
+  });
+});
+
 $(document).ready(function () {
   $("#sort-artist-button").click(function () {
     var posts = $("#blog-posts").children("article");
@@ -25,11 +75,5 @@ $(document).ready(function () {
       return yearA - yearB;
     });
     $("#blog-posts").empty().append(posts);
-  });
-});
-
-$(document).ready(function () {
-  $("#collapse-main").click(function () {
-    $("main").slideToggle("slow");
   });
 });
